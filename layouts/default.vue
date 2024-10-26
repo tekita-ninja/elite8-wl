0b744f070f37490384967e567065e18f26a777f1
uutbudiarto2012 <hikids.developer@gmail.com>
2024-10-26 14:29:10 +0700

    link

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,13 @@
     <main class="max-w-xl mx-auto px-5">
       <slot />
     </main>
-    <div class="fixed bottom-6 py-2 left-0 right-0 px-6 flex flex-col items-end gap-2 z-20">
-      <a href="http://ig" target="_blank" rel="noopener noreferrer">
+    <div class="max-w-xl mx-auto px-5 fixed bottom-6 py-2 left-0 right-0 flex flex-col items-end gap-2 z-20">
+      <a href="https://api.whatsapp.com/send?phone=6287869129888&text=Hallo%20Kak%20,%20Saya%20mau%20booking%20dong%20atas%20nama:%20"
+        target="_blank" rel="noopener noreferrer">
         <Icon class="h-8 w-8" name="logos:whatsapp-icon" />
       </a>
-      <a href="http://ig" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com/elite8_id/profilecard/?igsh=MTN1YnEwdmo5MXNs" target="_blank"
+        rel="noopener noreferrer">
         <Icon class="h-8 w-8" name="skill-icons:instagram" />
       </a>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="my-8">
     <div class="space-y-4">
-      <a href="https://drive.google.com/drive/folders/1qmk-xTu4Z08maOPPXsDURabVtITXBz38?usp=sharing" target="_blank"
+      <a href="https://drive.google.com/file/d/1G1liYRijRjM_1rj_l4WUsP8KW-xWvaXG/view?usp=drivesdk" target="_blank"
         rel="noopener noreferrer"
         class="flex p-5 border gap-x-2 items-center backdrop-blur border-slate-900 rounded-xl shadow shadow-[#CBAA61] bg-gradient-to-tr from-[#CBAA61]/20 via-slate-950/90 to-[#CBAA61]/50 text-slate-100">
         <Icon class="w-8 h-8 text-white" name="material-symbols:fastfood-rounded" />
         <p class="text-lg font-semibold">MENU</p>
       </a>
-      <NuxtLink
-      href="/waitlist"
+      <NuxtLink href="/waitlist"
         class="flex p-5 border gap-x-2 items-center backdrop-blur border-slate-900 rounded-xl shadow shadow-[#CBAA61] bg-gradient-to-tr from-[#CBAA61]/20 via-slate-950/90 to-[#CBAA61]/50 text-slate-100">
         <Icon class="w-8 h-8 text-white" name="fluent:document-bullet-list-clock-20-regular" />
         <p class="text-lg font-semibold">WAITLIST</p>
