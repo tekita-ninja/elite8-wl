2095f1b9615a8f1f010082d176ae2ac32ae41a16
uutbudiarto2012 <hikids.developer@gmail.com>
2024-12-01 13:30:50 +0700

    ok

--- components/WaitlistItem.vue
+++ components/WaitlistItem.vue
@@ -12,9 +12,15 @@ defineProps<{
       class="shrink-0 w-8 h-8 rounded-lg flex items-center justify-center bg-[#CBAA61] text-slate-950 text-xl font-bold">
       {{ number + 1 }}
     </div>
-    <div class="flex-1">
-      <div class="font-bold text-sm">{{ item.name }}</div>
-      <div class="text-sm">{{ subStringText(item.phoneNumber) }}</div>
+    <div class="flex-1 flex justify-between items-center">
+      <div>
+        <div class="font-bold text-sm">{{ item.name }}</div>
+        <div class="text-sm">{{ subStringText(item.phoneNumber) }}</div>
+      </div>
+      <div class="text-sm flex items-center gap-1">
+        <Icon name="ph:users-bold" />
+        {{ item.numOfPax }}
+      </div>
     </div>
   </div>
 </template>
