49e480954d19cd9fc98a16900c35fabd41d2d642
uutbudiarto2012 <hikids.developer@gmail.com>
2024-12-04 21:16:01 +0700

    ubah text peringatan

--- components/Announcement.vue
+++ components/Announcement.vue
@@ -5,7 +5,7 @@
   <div class="bg-slate-800/50 p-3 rounded backdrop-blur-lg border border-dashed border-red-500/30 text-red-500 z-30">
     <h2 class="text-sm">Peringatan!</h2>
     <p class="text-[12px]">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, quo sed autem architecto quibusdam illo ad.
+      Jika telah dipanggil melalui telpon/WA lebih dari 3x maka akan diskip ke player selanjutnya!
     </p>
   </div>
 </template>
